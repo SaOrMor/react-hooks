ec4357f1a7af05b6f07e418958eb0feb12e4a92e
salvatoremorise <sall.morise@gmail.com>
2021-05-28 11:39:37 +0200

    first commit

---
+++ react-hooks/src/App.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+
+function expensiveInitialState() {
+  return 10;
+}
+
+
+const  App = ()  => {
+  
+  useState(() => expensiveInitialState());
+  
+  return <div> hey</div> 
+};
+
+export default App;
